c98bab7c9aca1263d963a5431cb0b474e90d9c4a
SAman-9814 <131952420+SAman-9814@users.noreply.github.com>
2023-07-05 15:49:00 +0545

    webtask

---
+++ script.js
@@ -0,0 +1,174 @@
+//    darkmode
+
+const toggleicon = document.querySelector('.toggle-icon');
+
+toggleicon.addEventListener('click', () => {
+    toggleicon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+})
+
+
+// circle skill
+
+// const circles = document.querySelectorAll('.circle');
+// circles.forEach(elem=>{
+//     var dots = elem.getAttribute("data-dots");
+//     var marked = elem.getAttribute("data-percent");
+//     var percent = Math.floor(dots*marked/100);
+//     var points = "";
+//     var rotate = 360 / dots;
+
+
+//     for(let i = 0 ; i < dots ; i++){
+//         points += '<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>';
+//     }
+//     elem.innerHTML = points;
+
+//     const pointsMarked = elem.querySelectorAll('.points');
+//         for(let i = 0; i<percent ; i++){
+//            pointsMarked[i].classList.add('marked')
+//         }
+
+// })
+//     elem.innerHTML = points;
+
+//     const pointsMarked = elem.querySelectorAll('.points');
+//     for(let i = 0; i<percent ; i++){
+//         pointsMarked[i].classList.add('marked')
+//     }
+// })
+
+
+//   team member
+
+let imgBx = document.querySelectorAll('.imgBx');
+let contentBx = document.querySelectorAll('.contentBx');
+
+for (let i = 0; i < imgBx.length; i++) {
+    imgBx[i].addEventListener('mouseover', function () {
+        for (let i = 0; i < contentBx.length; i++) {
+            contentBx[i].className = 'contentBx';
+        }
+        document.getElementById(this.dataset.id).
+            className = 'contentBx active';
+
+        for (let i = 0; i < imgBx.length; i++) {
+            imgBx[i].className = 'imgBx';
+        }
+        this.className = 'imgBx active';
+    })
+}
+
+// toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+
+
+
+// scroll sections outline
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for Animation on scroll
+            // sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        // else {
+        //     sec.classList.remove('show-animate');
+        // }
+
+    });
+
+    // sticky navbar
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle and navbar when click navbar link (scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+// ----------------- typed js -----------------------
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Java Developer', 'Web Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+// ------------------------frontend animation-------------------------------
+
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () =>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i * 80);
+    });
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText,3000)
+
+// --------------------------------------------------------------
+
+// --------------- Emai js -----------------------------
+
+
+
+
